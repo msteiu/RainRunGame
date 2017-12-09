7126a9c92ea8dab9b8edb18460253dbd9ccb6a0f
msteiu <34246886+msteiu@users.noreply.github.com>
2017-12-09 12:18:47 -0500

    Update Monster.java

--- Monster.java
+++ Monster.java
@@ -7,56 +7,19 @@
 import java.awt.*;
 import java.util.Random;
 
-// if you uncomment lines above the monster will move in a zig zag;
-// not sure if thats more fun
-public class Monster extends MyCharacter implements Character {
-
-    // int moveAmt;
-    // boolean movingLeft;
+public class Monster extends MyCharacter implements FallingObject {
     
     public Monster(int xPos, int yPos, int size, Color color) {
-        super(xPos, yPos, size, color);
-        // this.moveAmt = new Random().nextInt(size / 3) + 1; // gentle drift
-        // this.movingLeft = false;
+        super(xPos, yPos, size, color, false);
     }
 
-    @Override
+    @Override 
     public String getType() {
         return "monster";
     }
-    //follow instead of moveUp? monster has different speed, so it couldnt use super() for moveUp
-    public void follow(MyCharacter me){
-        
+
+    @Override
+    public void moveDown(int speed) {
+      this.yPos += speed;
     }
-    // @Override
-    // public void moveRight() {
-    //     super.moveRight();
-    //     if ( (xPos + size) > rightEdge) {
-    //         movingLeft = true;
-    //     }
-    // }
-
-    // @Override
-    // public void moveLeft() {
-    //     super.moveLeft();
-    //     if ( (xPos - size) < leftEdge) {
-    //         movingLeft = false;
-    //     }
-    // }
-
-    // @Override
-    // public void moveDown() { //is MyCharacter supposed to move down as well? thought it'd only move forward
-    //     super.moveDown();
-    //     if (movingLeft) {
-    //         moveLeft();
-    //     } else {
-    //         moveRight();
-    //     }
-    // }
-
-    // @Override
-    // public void drawCharacter(Graphics g) {}
-
-    // @Override
-    // public Rectangle getBounds() {}
 }
