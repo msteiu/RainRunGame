a29bc3c8d97bb6f4ee4d68dffa364db921037af9
msteiu <34246886+msteiu@users.noreply.github.com>
2017-12-15 09:28:42 -0500

    Update UmbrellaPowerUp.java

--- UmbrellaPowerUp.java
+++ UmbrellaPowerUp.java
@@ -1,5 +1,15 @@
-public class UmbrellaPowerUp extends PowerUp {
+/**
+ * FILENAME: UmbrellaPowerUp
+ * DESCRIPTION: Powerup child implementing PowerUp abstract class. 
+ */
 
+public class UmbrellaPowerUp extends PowerUp {
+    /*
+     * Constructor of umbrella power-up
+     * @ param int X coordinate of location 
+     * @ param int Y coordinate of location 
+     * @ param String pathdir for icon used
+     */
     public UmbrellaPowerUp(int xCoord, int yCoord, String pathdir) {
         super(xCoord, yCoord, pathdir);
         powerType = "umbrella";
