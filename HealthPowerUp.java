7e0a028d131a4fe1d5047d024825c1f3282a72c0
msteiu <34246886+msteiu@users.noreply.github.com>
2017-12-15 08:49:15 -0500

    Update HealthPowerUp.java

--- HealthPowerUp.java
+++ HealthPowerUp.java
@@ -1,5 +1,16 @@
+/**
+ * FILENAME: Character
+ * DESCRIPTION: Powerup child implementing PowerUp abstract class. 
+ */
+
 public class HealthPowerUp extends PowerUp {
-  
+    
+    /*
+     * Constructor of health power-up
+     * @ param int X coordinate of location 
+     * @ param int Y coordinate of location 
+     * @ param String pathdir for icon used
+     */
     public HealthPowerUp (int xCoord, int yCoord, String pathdir) {
         super(xCoord, yCoord, pathdir);
         powerType = "health";
