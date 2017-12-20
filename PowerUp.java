fce8509d21ed2b54a892dc9a1d9ed200b4d36588
msteiu <34246886+msteiu@users.noreply.github.com>
2017-12-20 02:31:34 -0500

    Update PowerUp.java

--- PowerUp.java
+++ PowerUp.java
@@ -1,6 +1,8 @@
 /**
  * FILENAME: PowerUp
- * DESCRIPTION: CS230 Final Project - Rain Run Game
+ * DESCRIPTION: Abstract class which contains main characteristics and behaviors of the three power-ups: health, speed, umbrella. The 
+ * current class extends FallingObject abstract class, given that all power-ups are falling objects, but also has more specific charac-
+ * teristics than other falling objects such as rain drops (defined in RainDrop). 
  * @author Hunter Sessa, Angelina Li
  */
 
@@ -13,33 +15,34 @@ import java.util.Random;
 
 public abstract class PowerUp extends FallingObject {
     
+    //Instance variables of power-ups
     protected String powerType;
     private boolean movingLeft;
     private BufferedImage img;
     
     /**
-     * Constructor - defines what will be contained in PowerUp instance
-     * @param xCoord int- x position of character
-     * @param yCoord int- y position of character
-     * @param String imagePath for icon used
-     * inherits from FallingObject
+     * Constructor - defines what will be contained in every PowerUp instance; it inherits the constructor of FallingObject and additionally
+     * includes image representing power-up graphic (heart/shoe/umbrella).
+     * @param xCoord int- x coordinate of power-up location
+     * @param yCoord int- y coordinate of power-up location
+     * @param String imagePath- the String location of the icon used for each power-up
      */ 
     public PowerUp(int xCoord, int yCoord, String imagePath) {
         super(xCoord, yCoord);
 
         try {
-            img = ImageIO.read(new File(imagePath));
+            img = ImageIO.read(new File(imagePath)); //tries to access the image with the given name
             this.setWidth(img.getWidth());
             this.setHeight(img.getHeight());
         } catch (IOException e) {
-            System.out.println("Couldn't open image " + imagePath);
+            System.out.println("Couldn't open image " + imagePath); //if image couldn't be accessed, complain to user
         }
 
         this.movingLeft = new Random().nextBoolean();
     }
 
     /**
-     * getType() returns type of powerup 
+     * getType() returns type of powerup (umbrella/speed/health)
      * @return String type of powerup 
      */     
     public String getType() {
@@ -47,13 +50,13 @@ public abstract class PowerUp extends FallingObject {
     }
     
     /**
-     * drawCharacter() creates graphical representation of the PowerUp 
+     * drawCharacter() creates graphical representation of the PowerUp, making a power-up image appear on screen at a specific location
      */ 
     public void drawCharacter(Graphics g) {
         g.drawImage(img, getX(), getY(), new ImgObserver());
     }
 
-
+    
     public class ImgObserver implements ImageObserver {
 
         public boolean imageUpdate(Image img, int infoflags, int x, int y, int width, int height) {
