2acaab45917e0872e91add720c269cb5d0c34831
msteiu <34246886+msteiu@users.noreply.github.com>
2017-12-20 02:21:57 -0500

    Update HealthPowerUp.java

--- HealthPowerUp.java
+++ HealthPowerUp.java
@@ -1,21 +1,27 @@
 /**
  * FILENAME: HealthPowerUp
- * DESCRIPTION: Powerup child implementing PowerUp abstract class. 
+ * DESCRIPTION: Powerup child representing health (heart symbol) which increases the number of hearts the player has. It is extending the
+ * PowerUp abstract class. 
  * @author Angelina Li
  */
 
 public class HealthPowerUp extends PowerUp {
     /**
-     * Constructor of health power-up
+     * Constructor of a health power-up
      * @param int X coordinate of location 
      * @param int Y coordinate of location 
-     * @param int size to define which icon to use
+     * @param int size to define the size of the icon to use
      */
     public HealthPowerUp (int xCoord, int yCoord, int size) {
         super(xCoord, yCoord, getImagePath(size));
         powerType = "health";
     }
-
+    
+    /*
+     * Static method that returns the location of the image use for the representation of the health power-up (heart image).
+     * @param size int which represents the size of the image
+     * @return String representation of the image location
+     */
     private static String getImagePath(int size) {
         return "images/heart" + size + ".png";
     }
