dc484cf16b66d8f9d07d430a96c2b1c4a1a22b28
msteiu <34246886+msteiu@users.noreply.github.com>
2017-12-20 03:11:00 -0500

    Update SpeedPowerUp.java

--- SpeedPowerUp.java
+++ SpeedPowerUp.java
@@ -1,12 +1,12 @@
 /**
- * FILENAME: SoeedPowerUp
- * DESCRIPTION: Powerup child implementing PowerUp abstract class. 
+ * FILENAME: SpeedPowerUp
+ * DESCRIPTION: Speed power-up which extends PowerUp abstract class. The current power-up increases the speed of the player.
  * @author Angelina Li
  */
 
 public class SpeedPowerUp extends PowerUp {
     /**
-     * Constructor of speed power-up
+     * Constructor of speed power-up. It uses PowerUp's constructor, adding the specific type of the current power-up.
      * @param int X coordinate of location 
      * @param int Y coordinate of location 
      * @param int size to define which icon to use
@@ -16,6 +16,11 @@ public class SpeedPowerUp extends PowerUp {
         powerType = "speed";
     }
 
+    /*
+     * Static method that returns the String location of the speed image used within the game.
+     * @param int size represents size of icon
+     * @return String location of image
+     */
     private static String getImagePath(int size) {
         return "images/speed" + size + ".png";
     }
